be89f521d3ba089c4cb16f2450c4b7ae07c95c90
hacknature <info@hacknature.org>
2021-03-08 18:36:48 +0000

    Update file(s) "public/." from "hacknature/website"

---
+++ manifest.js
@@ -0,0 +1,19 @@
+{
+  "name": "HackNature",
+  "short_name": "hacknature",
+  "icons": [{
+    "src": "/img/icons/favicon-16x16.png",
+    "sizes": "16x16",
+    "type": "image/png"
+  }, {
+    "src": "/img/icons/favicon-32x32.png",
+    "sizes": "32x32",
+    "type": "image/png"
+
+  }],
+  "start_url": "/index.html",
+  "display": "standalone",
+  "orientation" : "portrait",
+  "background_color": "#000000",
+  "theme_color": "#000000"
+}
